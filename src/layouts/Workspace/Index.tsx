70e67a05a6bbc660575df5737d3cf74f4fa4c25f
kimtaenam <cjb940103@gmail.com>
2022-02-01 13:13:39 +0900

    workspace - gravatar, layouts

--- src/layouts/Workspace/Index.tsx
+++ src/layouts/Workspace/Index.tsx
@@ -1,8 +1,20 @@
+import {
+  Header,
+  RightMenu,
+  ProfileImg,
+  WorkspaceWrapper,
+  Workspaces,
+  Channels,
+  WorkspaceName,
+  Chats,
+  MenuScroll,
+} from '@layouts/Workspace/style';
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
 import React, { FC, useCallback } from 'react';
 import { Navigate } from 'react-router';
 import useSWR from 'swr';
+import gravatar from 'gravatar';
 
 const Workspace: FC = ({ children }) => {
   const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
@@ -23,8 +35,22 @@ const Workspace: FC = ({ children }) => {
 
   return (
     <div>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src={gravatar.url(data.nickname, { s: '28px', d: 'retro' })} alt="data.nickname" />
+          </span>
+        </RightMenu>
+      </Header>
       <button onClick={onLogOut}>로그아웃</button>
-      {children}
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>sleact</WorkspaceName>
+          <MenuScroll>menu scroll</MenuScroll>
+        </Channels>
+        <Chats>Chats</Chats>
+      </WorkspaceWrapper>
     </div>
   );
 };
