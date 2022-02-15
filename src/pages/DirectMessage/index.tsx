d383db8c719b0aca812ccd539783364b084f0565
kimtaenam <cjb940103@gmail.com>
2022-02-15 23:26:03 +0900

    채팅 보내기

--- src/pages/DirectMessage/index.tsx
+++ src/pages/DirectMessage/index.tsx
@@ -7,17 +7,33 @@ import gravatar from 'gravatar';
 import ChatList from '@components/ChatList';
 import ChatBox from '@components/ChatBox';
 import useInput from '@hooks/useInput';
+import axios from 'axios';
+import { IDM } from '@typings/db';
 
+const PAGE_SIZE = 20;
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: myData } = useSWR('/api/users', fetcher);
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const [chat, onChangeChat, setChat] = useInput('');
+  const { data: chatData, mutate: mutateChat } = useSWR<IDM[]>(
+    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=${PAGE_SIZE}&page=1}`,
+    fetcher,
+  );
 
-  const onSubmitForm = useCallback((e) => {
-    e.prevertDefault();
-    console.log(100);
-  }, []);
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (chat?.trim() && chatData) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .catch(console.error);
+      }
+    },
+    [chat, workspace, id, myData, userData, chatData, mutateChat, setChat],
+  );
 
   if (!userData || !myData) {
     return null;
