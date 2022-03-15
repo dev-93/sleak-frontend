9c98533b760e0faa6421661e2701b8892ad30d1e
kimtaenam <cjb940103@gmail.com>
2022-03-15 16:13:00 +0900

    socket 부분 정리

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,11 +1,11 @@
 import { useCallback } from 'react';
-import io from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 
 const backUrl = process.env.NODE_ENV === 'production' ? 'https://sleact.nodebird.com' : 'http://localhost:3095';
 
-const sockets: { [key: string]: SocketIOClient.Socket } = {};
+const sockets: { [key: string]: Socket } = {};
 
-const useSocket = (workspace?: string): [SocketIOClient.Socket | undefined, () => void] => {
+const useSocket = (workspace?: string): [Socket | undefined, () => void] => {
   const disconnect = useCallback(() => {
     if (workspace && sockets[workspace]) {
       sockets[workspace].disconnect();
